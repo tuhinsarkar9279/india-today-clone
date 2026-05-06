987f8ba919e5bffb3e4bec9e6fcdcc7d7525291c
tuhinsarkar9279 <tuhinsarkar9279@gmail.com>
2026-05-06 16:18:39 +0530

    4-5-26

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function toggleSidebar(){
+
+    let sidebar = document.getElementById("sidebar");
+    let main = document.getElementById("main");
+    let bar = document.getElementById("zzz");
+
+    if(sidebar.style.display === "none"){
+        sidebar.style.display = "block";
+        
+        sidebar.style.backgroundColor = "rgb(239,243,246)";
+        main.style.position = "relative";
+        sidebar.style.position = "absolute";
+        sidebar.style.height = "92%";
+        sidebar.style.position = "fixed";
+        document.body.style.overflow = "hidden";
+        sidebar.style.overflowX = "scroll";
+
+
+
+    }
+    else{
+        sidebar.style.display = "none";
+         document.body.style.overflow = "auto";
+        
+    }
+
+}
